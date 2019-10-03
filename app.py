adf389cc0c2ac3652abb3c74d5c884d9ab85f773
hschallh <hschallh@gmail.com>
2019-10-03 18:21:09 -0400

    Linting changes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+""" Start of the application"""
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # pylint: disable=invalid-name
 
 
 @app.route('/')
 def hello_world():
+    """ Returns 'Hello World' """
     return 'Hello World!'
 
 
