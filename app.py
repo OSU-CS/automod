44b253abcfb45223add20d4c1ab42abc4f4d5a67
hschallh <hschallh@gmail.com>
2019-12-05 22:56:59 -0500

    Don't break up commits into digestable changes
    - Change naming of "channel" to "report_channel" to clarify functions
      that may have multiple variables that refer to channels
    - Add message formatting for new channels
    - Subscribe to new channel events
    - Report to the admin channel when a new channel is created

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 
 import os
 import slack
+
 from emoji_message import EmojiMessage
+from new_channel_message import NewChannelMessage
 
 
 @slack.RTMClient.run_on(event='emoji_changed')
@@ -12,23 +14,47 @@ def emoji_callback(**payload):
     Triggered when an emoji is added, removed, or when a new alias has been created.
     """
     web_client = payload['web_client']
-
     event_type = payload['data']['subtype']
     emoji_name = payload['data']['name'] if event_type == 'add' else payload['data']['names'][0]
 
     send_emoji_message(web_client, 'admin', emoji_name, event_type)
 
 
-def send_emoji_message(web_client: slack.WebClient, channel: str, emoji_name: str, event_type: str):
+@slack.RTMClient.run_on(event='channel_created')
+def new_channel_callback(**payload):
+    """Catch channel_created event.
+
+    Triggered when a channel has been created.
+    """
+    web_client = payload['web_client']
+    new_channel_name = payload['data']['channel']['name']
+
+    send_new_channel_message(web_client, 'admin', new_channel_name)
+
+
+def send_emoji_message(web_client: slack.WebClient, report_channel: str, emoji_name: str, event_type: str):
     """Send a message to a channel about an emoji event.
 
     :param web_client: The client to respond on
-    :param channel: The channel to send the message to
+    :param report_channel: The channel to send the message to
     :param emoji_name: The name of the emoji
     :param event_type: The event that has happened, 'add' or 'remove'
     """
-    emoji_message = EmojiMessage(channel, emoji_name, event_type)
+    emoji_message = EmojiMessage(report_channel, emoji_name, event_type)
     message = emoji_message.get_message_payload()
+
+    web_client.chat_postMessage(**message)
+
+
+def send_new_channel_message(web_client: slack.WebClient, report_channel: str, new_channel_name: str):
+    """Send a message to a channel about a new channel event.
+
+    :param web_client: The client to respond on
+    :param report_channel: The channel to send the message to
+    :param new_channel_name: The name of the created channel
+    """
+    new_channel_message = NewChannelMessage(report_channel, new_channel_name)
+    message = new_channel_message.get_message_payload()
 
     web_client.chat_postMessage(**message)
 
